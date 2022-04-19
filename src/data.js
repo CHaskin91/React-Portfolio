a9badb35d1af2f94280b5d19370c7d3a38baed3d
Chubbs91 <chaskin1991@outlook.com>
2022-04-18 21:34:51 -0500

    casing fix

--- src/data.js
+++ src/data.js
@@ -2,32 +2,32 @@ export const featuredPortfolio = [
     {
         id: 1,
         title: "Homework Tracker",
-        img: "/assets/HWtracker.png",
+        img: "/assets/HWtracker.PNG",
     },
     {
         id: 2,
         title: "Movie Night",
-        img: "/assets/movieNight.png",
+        img: "/assets/movieNight.PNG",
     },
     {
         id: 3,
         title: "Photo Port",
-        img: "/assets/photoPort.png",
+        img: "/assets/photoPort.PNG",
     },
     {
         id: 4,
         title: "Pizza Hunt",
-        img: "/assets/pizzaHunt.png",
+        img: "/assets/pizzaHunt.PNG",
     },
     {
         id: 5,
         title: "Weather Forecaster",
-        img: "/assets/weather.png",
+        img: "/assets/weather.PNG",
     },
     {
         id: 6,
         title: "Tech Blogger",
-        img: "/assets/techBlogger.png",
+        img: "/assets/techBlogger.PNG",
     }
 ];
 
@@ -36,12 +36,12 @@ export const reactPortfolio = [
         id: 1,
         title: "React Portfolio",
         // tentative photo
-        img: "/assets/foodfestival.png",
+        img: "/assets/foodfestival.PNG",
     },
     {
         id: 2,
         title: "Photo Port",
-        img: "/assets/photoPort.png",
+        img: "/assets/photoPort.PNG",
     },
     {
         id: 3,
@@ -69,31 +69,31 @@ export const allPortfolio = [
     {
         id: 1,
         title: "Budget Tracker",
-        img: "/assets/budget.png",
+        img: "/assets/budget.PNG",
     },
     {
         id: 2,
         title: "Food Festival",
-        img: "/assets/foodFestival.png",
+        img: "/assets/foodFestival.PNG",
     },
     {
         id: 3,
         title: "My First Portfolio",
-        img: "/assets/firstPortfolio.png",
+        img: "/assets/firstPortfolio.PNG",
     },
     {
         id: 4,
         title: "eCommerce Back-End",
-        img: "/assets/eCommerce.png",
+        img: "/assets/eCommerce.PNG",
     },
     {
         id: 5,
         title: "My Note Taker",
-        img: "/assets/noteTaker.png",
+        img: "/assets/noteTaker.PNG",
     },
     {
         id: 6,
         title: "Work Day Scheduler",
-        img: "/assets/workScheduler.png",
+        img: "/assets/workScheduler.PNG",
     },
 ]

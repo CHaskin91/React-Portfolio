53807d99280af4844a37c8e1ebff1cc6cb4351b7
Chubbs91 <chaskin1991@outlook.com>
2022-05-04 15:28:09 -0500

    update portfolio list

--- src/data.js
+++ src/data.js
@@ -1,99 +1,99 @@
 export const featuredPortfolio = [
-    {
-        id: 1,
-        title: "Homework Tracker",
-        img: "/assets/HWtracker.PNG",
-    },
-    {
-        id: 2,
-        title: "Movie Night",
-        img: "/assets/movieNight.PNG",
-    },
-    {
-        id: 3,
-        title: "Photo Port",
-        img: "/assets/photoPort.PNG",
-    },
-    {
-        id: 4,
-        title: "Pizza Hunt",
-        img: "/assets/pizzaHunt.PNG",
-    },
-    {
-        id: 5,
-        title: "Weather Forecaster",
-        img: "/assets/weather.PNG",
-    },
-    {
-        id: 6,
-        title: "Tech Blogger",
-        img: "/assets/techBlogger.PNG",
-    }
+  {
+    id: 1,
+    title: "Homework Tracker",
+    img: "/assets/HWtracker.PNG",
+  },
+  {
+    id: 2,
+    title: "Movie Night",
+    img: "/assets/movieNight.PNG",
+  },
+  {
+    id: 3,
+    title: "AC Pizza",
+    img: "/assets/ac-pizza.PNG",
+  },
+  {
+    id: 4,
+    title: "Photo Port",
+    img: "/assets/photoPort.PNG",
+  },
+  {
+    id: 5,
+    title: "Weather Forecaster",
+    img: "/assets/weather.PNG",
+  },
+  {
+    id: 6,
+    title: "Tech Blogger",
+    img: "/assets/techBlogger.PNG",
+  },
 ];
 
 export const reactPortfolio = [
-    {
-        id: 1,
-        title: "React Portfolio",
-        // tentative photo
-        img: "/assets/foodfestival.PNG",
-    },
-    {
-        id: 2,
-        title: "Photo Port",
-        img: "/assets/photoPort.PNG",
-    },
-    {
-        id: 3,
-        title: "Coming Soon...",
-        img: "/assets/stayTuned.jpg",
-    },
-    {
-        id: 4,
-        title: "Coming Soon...",
-        img: "/assets/stayTuned.jpg",
-    },
-    {
-        id: 5,
-        title: "Coming Soon...",
-        img: "/assets/stayTuned.jpg",
-    },
-    {
-        id: 6,
-        title: "Coming Soon...",
-        img: "/assets/stayTuned.jpg",
-    }
+  {
+    id: 1,
+    title: "React Portfolio",
+    // tentative photo
+    img: "/assets/react-port.PNG",
+  },
+  {
+    id: 2,
+    title: "AC Pizza",
+    img: "/assets/ac-pizza.PNG",
+  },
+  {
+    id: 3,
+    title: "Photo Port",
+    img: "/assets/photoPort.PNG",
+  },
+  {
+    id: 4,
+    title: "Coming Soon...",
+    img: "/assets/stayTuned.jpg",
+  },
+  {
+    id: 5,
+    title: "Coming Soon...",
+    img: "/assets/stayTuned.jpg",
+  },
+  {
+    id: 6,
+    title: "Coming Soon...",
+    img: "/assets/stayTuned.jpg",
+  },
 ];
 
 export const allPortfolio = [
-    {
-        id: 1,
-        title: "Budget Tracker",
-        img: "/assets/budget.PNG",
-    },
-    {
-        id: 2,
-        title: "Food Festival",
-        img: "/assets/foodFestival.PNG",
-    },
-    {
-        id: 3,
-        title: "My First Portfolio",
-        img: "/assets/firstPortfolio.PNG",
-    },
-    {
-        id: 4,
-        title: "eCommerce Back-End",
-        img: "/assets/eCommerce.PNG",
-    },
-    {
-        id: 5,
-        title: "My Note Taker",
-        img: "/assets/noteTaker.PNG",
-    },
-    {
-        id: 6,
-        title: "Work Day Scheduler",
-        img: "/assets/workScheduler.PNG",
-    },
-]
+  {
+    id: 1,
+    title: "Budget Tracker",
+    img: "/assets/budget.PNG",
+  },
+  {
+    id: 2,
+    title: "Food Festival",
+    img: "/assets/foodFestival.PNG",
+  },
+  {
+    id: 3,
+    title: "My First Portfolio",
+    img: "/assets/firstPortfolio.PNG",
+  },
+  {
+    id: 4,
+    title: "eCommerce Back-End",
+    img: "/assets/eCommerce.PNG",
+  },
+  {
+    id: 5,
+    title: "My Note Taker",
+    img: "/assets/noteTaker.PNG",
+  },
+  {
+    id: 6,
+    title: "Work Day Scheduler",
+    img: "/assets/workScheduler.PNG",
+  },
+];
